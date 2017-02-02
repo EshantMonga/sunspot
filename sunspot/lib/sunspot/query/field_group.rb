b9039aeed1da7b80f96a9cc0655e85d26b54715b
Tabs <ryan.cooke@gmail.com>
2017-02-02 18:50:29 -0500

    make ngroups param settable in sunspot dsl

--- sunspot/lib/sunspot/query/field_group.rb
+++ sunspot/lib/sunspot/query/field_group.rb
@@ -4,13 +4,14 @@ module Sunspot
     # A FieldGroup groups by the unique values of a given field.
     #
     class FieldGroup
-      attr_accessor :limit, :truncate
+      attr_accessor :limit, :truncate, :ngroups
 
       def initialize(field)
         if field.multiple?
           raise(ArgumentError, "#{field.name} cannot be used for grouping because it is a multiple-value field")
         end
         @field = field
+        @ngroups = false
 
         @sort = SortComposite.new
       end
@@ -22,7 +23,7 @@ module Sunspot
       def to_params
         params = {
           :group            => "true",
-          :"group.ngroups"  => "false",
+          :"group.ngroups"  => @ngroups,
           :"group.field"    => @field.indexed_name
         }
 
