1d6b7ed672d6f0f9e512ac61e72d55b83d02b30d
INDIA\emonga <>
2022-06-29 15:26:21 +0530

    filter keyword is replaced by action in rails 5.

--- sunspot_rails/lib/sunspot/rails/request_lifecycle.rb
+++ sunspot_rails/lib/sunspot/rails/request_lifecycle.rb
@@ -17,11 +17,11 @@ module Sunspot #:nodoc:
           loaded_controllers = [base].concat(subclasses)
           # Depending on how Sunspot::Rails is loaded, there may already be
           # controllers loaded into memory that subclass this controller. In
-          # this case, since after_filter uses the inheritable_attribute
+          # this case, since after_action uses the inheritable_attribute
           # structure, the already-loaded subclasses don't get the filters. So,
           # the below ensures that all loaded controllers have the filter.
           loaded_controllers.each do |controller|
-            controller.after_filter do
+            controller.after_action do
               if Sunspot::Rails.configuration.auto_commit_after_request?
                 Sunspot.commit_if_dirty
               elsif Sunspot::Rails.configuration.auto_commit_after_delete_request?
